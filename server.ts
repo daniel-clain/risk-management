a36d80d349a28a9230a62d76b683c79010f66cb0
Daniel <daniel.clain@hotmail.com>
2024-03-05 09:10:08 +1100

    small fix

--- server.ts
+++ server.ts
@@ -1,35 +1,29 @@
-import * as express from 'express'
-import { writeFileSync, readFileSync  } from 'fs';
-import {urlencoded, json} from 'body-parser';
+import { json, urlencoded } from "body-parser";
+import * as express from "express";
+import { readFileSync, writeFileSync } from "fs";
 
 const app = express();
-const saveGameFilePath = `${__dirname}/public/saved-game.json`
+const saveGameFilePath = `${__dirname}/public/saved-game.json`;
 
+app.use(express.static("dist"));
+app.use(urlencoded({ extended: false }));
 
-app.use(express.static('dist'));
-app.use(urlencoded({ extended: false }))
+app.post("/save-game", json(), (req, res) => {
+  const saveGameJson = req.body;
+  console.log("save-game called");
+  const saveGameFile = JSON.stringify(saveGameJson);
+  writeFileSync(saveGameFilePath, saveGameFile);
+  res.send("penis");
+});
 
+app.get("/load-game", (req, res) => {
+  console.log("load-game called");
 
+  const saveGameFile = readFileSync(saveGameFilePath, "utf8");
+  const saveGameJson = JSON.parse(saveGameFile);
+  res.header("Content-Type", "application/json");
+  res.send(saveGameJson);
+});
 
-app.post('/save-game', json(), (req, res) => {
-  const saveGameJson = req.body
-  console.log('save-game called');
-  const saveGameFile = JSON.stringify(saveGameJson)
-  writeFileSync(saveGameFilePath, saveGameFile)
-  res.send('penis')
-
-})
-
-app.get('/load-game', (req, res) => {  
-  console.log('load-game called');
-  
-  const saveGameFile = readFileSync(saveGameFilePath, 'utf8')
-  const saveGameJson = JSON.parse(saveGameFile)
-  res.header("Content-Type",'application/json');
-  res.send(saveGameJson)
-})
-
-const PORT = 3000;
+const PORT = 6969;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
